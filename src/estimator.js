72e2b31329b582450744fd2d8aa10b2e4519ba00
root <root@NAFIU.localdomain>
2020-04-07 22:59:05 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,30 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  };
+
+  const estimateCurrentlyInfected = {
+    currentlyInfected: input.reportedCases * 10
+  };
+
+  const estimateProjectedInfection = {
+    currentlyInfected: input.reportedCases * 50
+  };
+  return {
+    data: input,
+    impact: estimateCurrentlyInfected,
+    severeImpact: estimateProjectedInfection
+  };
+};
 
 export default covid19ImpactEstimator;
