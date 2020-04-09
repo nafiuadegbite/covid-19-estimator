89a93dc361f0c1cad49e9d2e725e5a3923f99a8a
nafiuadegbite <youngguru23@gmail.com>
2020-04-09 06:39:54 +0100

    fix challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,27 @@
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases } = data;
+  const { reportedCases, timeToElapse, periodType } = data;
+
+  let factor;
+  // let period;
+
+  if (periodType === 'days') {
+    factor = timeToElapse / 3;
+    // period = timeToElapse;
+  } else if (periodType === 'weeks') {
+    factor = (timeToElapse * 7) / 3;
+    // period = timeToElapse * 7;
+  } else {
+    factor = (timeToElapse * 30) / 3;
+    // period = timeToElapse * 30;
+  }
 
   // impact estimation
   const impactCurrentlyInfected = reportedCases * 10;
-  const impactInfectionsByRequestedTime = impactCurrentlyInfected * 2 ** 9;
+  const impactInfectionsByRequestedTime = impactCurrentlyInfected * 2 ** factor;
 
   // severeImpact estimation
   const severeImpactCurrentlyInfected = reportedCases * 50;
-  const severeImpactInfectionsByRequestedTime = severeImpactCurrentlyInfected * 2 ** 9;
+  const severeImpactInfectionsByRequestedTime = severeImpactCurrentlyInfected * 2 ** factor;
 
   const impact = {
     currentlyInfected: impactCurrentlyInfected,
